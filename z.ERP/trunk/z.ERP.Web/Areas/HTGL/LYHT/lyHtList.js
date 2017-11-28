be79baaff1cee572741dad2f75b04a4ba143cb6d
LINAJ\Administrator <linaj@changyi.com>
2017-11-28 22:47:28 +0800

    1

---
+++ z.ERP/trunk/z.ERP.Web/Areas/HTGL/LYHT/lyHtList.js
@@ -0,0 +1,12 @@
+﻿var Search = new Vue({
+    el: "#List_Main",
+    data: {
+        SearchData: {
+            NAME: ''
+        },
+        colBRAND: [
+            { title: '名称', key: 'NAME' },
+            { title: '名称', key: 'DEPT_NAME' }
+        ]
+    }
+})
