d000a1ec0a65e67cdbf602e1096b702a8e465603
linaj <41703243@qq.com>
2017-12-05 23:50:27 +0800

    定义模板

--- z.ERP/trunk/z.ERP.Web/Areas/XTGL/PAY/Pay.js
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/PAY/Pay.js
@@ -1,4 +1,11 @@
-﻿
+﻿define.beforeVue = function () {
+    colPay = [
+        { title: '支付方式CODE', key: 'CODE', width: 150 },
+        { title: '支付方式名称', key: 'NAME', width: 250 }];
 
+    dataPay = [];
+}
 
-
+define.afterSave = function (data) {
+    dataPay = data.row;
+}
