b62af73559e5e2331e48593b7cdf0c82f561e623
shizh <shizh@SZHPC>
2017-12-08 16:52:11 +0800

    增加付款方式

--- z.ERP/trunk/z.ERP.Services/TestService.cs
+++ z.ERP/trunk/z.ERP.Services/TestService.cs
@@ -105,5 +105,12 @@ namespace z.ERP.Services
             DataTable dt = DbHelper.ExecuteTable(sql, item.PageInfo, out count);
             return new DataGridResult(dt, count);
         }
+
+        public DataGridResult GetFkfs(SearchItem item) {
+            string sql = $@"select * from FKFS";
+            int count;
+            DataTable dt = DbHelper.ExecuteTable(sql, item.PageInfo, out count);
+            return new DataGridResult(dt, count);
+        }
     }
 }
---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/FKFS/Fkfs.js
@@ -0,0 +1,19 @@
+﻿define.beforeVue = function () {
+    define.screenParam.colFkfs = [
+        { title: '代码', key: 'ID', width: 150 },
+        { title: '名称', key: 'NAME', width: 150 },
+    ]
+    define.screenParam.dataFkfs = [];
+}
+
+define.search = function ()
+{
+    _.Search({
+        Service: "TestService",
+        Method: "GetFkfs",
+        Data: {},
+        Success: function (data) {
+            define.screenParam.dataFkfs = data.rows;
+        }
+    })
+}
