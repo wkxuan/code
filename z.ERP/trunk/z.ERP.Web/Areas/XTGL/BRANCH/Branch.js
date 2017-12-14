548856f986869eb99294ca53a6cd46a13ea54b5d
linaj <41703243@qq.com>
2017-12-14 12:36:52 +0800

    枚举类型合并一个文件

--- z.ERP/trunk/z.ERP.Web/Areas/XTGL/BRANCH/Branch.js
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/BRANCH/Branch.js
@@ -1,1 +1,30 @@
-﻿
+﻿define.beforeVue = function () {
+
+    define.screenParam.colDef = [
+        {
+            title: "分店代码",
+            key: 'ID', width: 150
+        },
+        {
+            title: '门店名称',
+            key: 'NAME', width: 250
+        }];
+
+    define.screenParam.dataDef = [];
+}
+
+
+define.search = function () {
+    _.Search({
+        Service: 'TestService',
+        Method: 'GetBranch',
+        Data: {},
+        Success: function (data) {
+            define.screenParam.dataDef = data.rows;
+        }
+    })
+}
+
+define.newRecord = function () {
+    define.dataParam.STATUS = "1";
+}
---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/BRANCH/BranchController.cs
@@ -0,0 +1,13 @@
+﻿using z.ERP.Web.Areas.Base;
+using System.Web.Mvc;
+
+namespace z.ERP.Web.Areas.XTGL.BRANCH
+{
+    public class BranchController: BaseController
+    {
+        public ActionResult Branch()
+        {
+            return View();
+        }
+    }
+}
