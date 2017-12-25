45ba578472af4d674f36a5514014612b99c8c066
linaj <41703243@qq.com>
2017-12-25 23:56:27 +0800

    定义模板:号,分店js重新添加

---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/BRANCH/Branch.js
@@ -0,0 +1,28 @@
+﻿define.beforeVue = function () {
+    define.screenParam.colDef = [
+        {
+            title: '分店代码',
+            key: 'ID', width: 150,
+        },
+        {
+            title: '分店名称',
+            key: 'NAME', width: 250,
+        }];
+
+    define.screenParam.dataDef = [];
+    define.windowParam = {
+        terst: false
+    }
+
+    define.service = "XtglService";
+    define.method = "GetBranchElement";
+    define.methodList = "GetBranch";
+    define.Key = 'ID';
+}
+
+
+
+define.newRecord = function () {
+    define.dataParam.STATUS = "1";
+}
+
