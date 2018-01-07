44f8b6a8f3d6fe88d047fe49779493689077eb64
shizh <shizh@SZHPC>
2018-01-08 00:46:28 +0800

    完善程序

--- z.ERP/trunk/z.ERP.Services/WyglService.cs
+++ z.ERP/trunk/z.ERP.Services/WyglService.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using z.ERP.Entities;
+using z.ERP.Entities.Enum;
+using z.Extensions;
 using z.MVC5.Results;
 
 namespace z.ERP.Services
@@ -25,6 +27,33 @@ namespace z.ERP.Services
             int count;
             DataTable dt = DbHelper.ExecuteTable(sql, item.PageInfo, out count);
             return new DataGridResult(dt, count);
+        }
+
+        public string SaveEnergyreGister(ENERGY_REGISTEREntity SaveData)
+        {
+            var v = GetVerify(SaveData);
+            if (SaveData.BILLID.IsEmpty())
+            {
+                SaveData.BILLID = NewINC("ENERGY_REGISTER");
+            }
+            SaveData.REPORTER = employee.Id;
+            SaveData.REPORTER_NAME = employee.Name;
+            SaveData.REPORTER_TIME = DateTime.Now.ToString();
+            SaveData.STATUS = ((int)普通单据状态.未审核).ToString();
+            v.Require(a => a.BILLID);
+            v.Require(a => a.CHECK_DATE);
+            v.Require(a => a.YEARMONTH);
+            v.Verify();
+
+            SaveData.ENERGY_REGISTER_ITEM.ForEach(sdb =>
+            {
+                GetVerify(sdb).Require(a => a.BILLID);
+                GetVerify(sdb).Require(a => a.FILEID);
+                GetVerify(sdb).Require(a => a.AMOUNT);
+            });
+            v.Verify();
+            DbHelper.Save(SaveData);
+            return SaveData.BILLID;
         }
 
 
--- z.ERP/trunk/z.ERP.Web/Areas/WYGL/ENERGYREGISTER/EnergyregisterEdit.js
+++ z.ERP/trunk/z.ERP.Web/Areas/WYGL/ENERGYREGISTER/EnergyregisterEdit.js
@@ -1,1 +1,78 @@
-﻿
+﻿editDetail.beforeVue = function () {
+
+    editDetail.service = "WyglService";
+    editDetail.method = "GetEnergyreGister"
+    editDetail.Key = 'BILLID';
+
+    editDetail.screenParam.colDef = [
+        {
+            title: "设备编码", key: "FILEID", width: 100,
+            render: function (h, params) {
+                return h('Input', {
+                    props: {
+                        value: params.FILEID
+                    },
+                    on: {
+                        'on-blur': function (event) {
+                            editDetail.dataParam.ENERGY_REGISTER_ITEM[params.index].FILEID = event.target.value;
+                        }
+                    }
+                })
+            }
+        },
+        { title: "序号", key: "INX", width: 100, },
+        { title: "商铺ID", key: "SHOPID", width: 100, },
+        { title: "商铺代码", key: "SHOPDM", width: 100, },
+        { title: "租约号", key: "CONTRACTID", width: 100, },
+        { title: "上次读数", key: "VALUE_LAST", width: 100, },
+        { title: "当前读数", key: "VALUE_CURRENT", width: 100, },
+        { title: "使用量", key: "VALUE_USE", width: 100, },
+        { title: "单价", key: "PRICE", width: 100, },
+        { title: "金额", key: "AMOUNT", width: 100, },
+        { title: "开始日期", key: "START_DATE", width: 100, },
+        { title: "结束日期", key: "END_DATE", width: 100, },
+        {
+            title: '操作',
+            key: 'action',
+            width: 80,
+            align: 'center',
+            render: function (h, params) {
+                return h('div',
+                    [
+                    h('Button', {
+                        props: { type: 'primary', size: 'small', disabled: false },
+
+                        style: { marginRight: '50px' },
+                        on: {
+                            click: function (event) {
+                                editDetail.dataParam.ENERGY_REGISTER_ITEM.splice(params.index, 1);
+                            }
+                        },
+                    }, '删除')
+                    ]);
+            }
+        }
+
+    ]
+
+    if (!editDetail.dataParam.ENERGY_REGISTER_ITEM) {
+        editDetail.dataParam.ENERGY_REGISTER_ITEM = [{
+            FILEID: "1",
+            INX: "1",
+            SHOPID: "1",
+            CONTRACTID: "1",
+            VALUE_LAST:"0",
+            VALUE_CURRENT: "100",
+            VALUE_USE: "100",
+            PRICE:"1.2",
+            AMOUNT:"120",
+            START_DATE:"2018.1.1",
+            END_DATE:"2018.1.7",
+        }]
+    }
+    editDetail.screenParam.addCol = function () {
+        var temp = editDetail.dataParam.ENERGY_REGISTER_ITEM || [];
+        temp.push({});
+        editDetail.dataParam.ENERGY_REGISTER_ITEM = temp;
+    }
+}
