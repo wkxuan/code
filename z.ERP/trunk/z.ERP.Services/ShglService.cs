d1bf9b905a73482a5a32afa9eb6f3b6c9ffc7647
linaj <41703243@qq.com>
2018-01-19 00:32:38 +0800

    好多地方需要完善

--- z.ERP/trunk/z.ERP.Services/ShglService.cs
+++ z.ERP/trunk/z.ERP.Services/ShglService.cs
@@ -55,13 +55,24 @@ namespace z.ERP.Services
         }
 
 
+
+        //public class MERCHANTModel : MERCHANTEntity
+        //{
+        //    public List<MERCHANT_BRANDModel> MERCHANT_BRAN;
+        //}
+        //public class MERCHANT_BRANDModel : MERCHANT_BRANDEntity
+        //{
+        //    public string NAME { get; set; }
+        //}
+
         public object GetMerchantElement(MERCHANTEntity Data)
         {
-             //此处校验一次只能查询一个单号,校验单号必须存在
+            //此处校验一次只能查询一个单号,校验单号必须存在
             string sql = $@"SELECT * FROM MERCHANT WHERE 1=1 ";
             if (!Data.MERCHANTID.IsEmpty())
                 sql += (" AND MERCHANTID= " + Data.MERCHANTID);
-            DataTable dt = DbHelper.ExecuteTable(sql);
+            //var Merchant = DbHelper.ExecuteObject<MERCHANTModel>(sql);
+            DataTable merchant = DbHelper.ExecuteTable(sql);
 
 
 
@@ -70,21 +81,32 @@ namespace z.ERP.Services
                 " where M.MERCHANTID = E.MERCHANTID AND M.BRANDID=C.ID";
             if (!Data.MERCHANTID.IsEmpty())
                 sqlitem += (" and E.MERCHANTID= " + Data.MERCHANTID);
-            DataTable dtitem = DbHelper.ExecuteTable(sqlitem);
+            //Merchant[0].MERCHANT_BRAN = DbHelper.ExecuteObject<MERCHANT_BRANDModel>(sqlitem);
+            DataTable merchantBrand = DbHelper.ExecuteTable(sqlitem);
 
             var result = new
             {
-                MERCHANTID = dt.Rows[0]["MERCHANTID"].ToString(),
-                NAME = dt.Rows[0]["NAME"].ToString(),
-                MERCHANT_BRAND = new dynamic[]
-                {
-                    new
-                    {
-                        BRANDID = dtitem.Rows[0]["BRANDID"].ToString(),
-                        NAME = dtitem.Rows[0]["NAME"].ToString(),
-                    }
-                }
+                //Merchant,
+                //主表肯定是一条记录
+                MERCHANTID = merchant.Rows[0]["MERCHANTID"].ToString(),
+                NAME = merchant.Rows[0]["NAME"].ToString(),
+                BILLID = merchant.Rows[0]["MERCHANTID"].ToString(),
+                SH = merchant.Rows[0]["SH"].ToString(),
+                BANK = merchant.Rows[0]["BANK"].ToString(),
+                //MERCHANT_BRAND = new dynamic[] {
+                //    NAME=merchantBrand.Rows[0]["NAME"].ToString(),
+                //for (var i=0; i<=merchantBrand.Rows.Count - 1; i++)
+                //{
+                //    NAME = merchantBrand.Rows[i]["NAME"].ToString()
+                //}
+                //},
             };
+
+            //merchant.Rows,
+            //MERCHANT_BRAND = new dynamic[] {
+            //   merchantBrand.Rows
+            //}
+            //return Merchant;
             return result;
         }
     }
