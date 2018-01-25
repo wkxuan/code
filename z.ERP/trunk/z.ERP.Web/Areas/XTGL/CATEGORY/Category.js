ac0fe685e76cb4d865bc54abaf87ee5dcc9a1888
shizh <shizh@SZHPC>
2018-01-25 12:39:09 +0800

    业态

---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/CATEGORY/Category.js
@@ -0,0 +1,10 @@
+﻿define.beforeVue = function () {
+    define.screenParam.dataDef = [];
+
+    define.service = "XtglService";
+    define.method = "TreeCategoryData";
+    define.methodList = "TreeCategoryList";
+    define.Key = 'CATEGORYCODE';
+}
+
+
---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/CATEGORY/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using z.ERP.Entities;
+using z.ERP.Model.Vue;
+using z.ERP.Web.Areas.Base;
+
+namespace z.ERP.Web.Areas.XTGL.CATEGORY
+{
+    public class CategoryController: BaseController
+    {
+        public ActionResult Category()
+        {
+            ViewBag.Tiele = "业态信息维护";
+            return View();
+        }
+        public string Save(string Tar, string Key, CATEGORYEntity DefineSave)
+        {
+            var allenum = SelectList(new CATEGORYEntity());
+            string newkey = TreeModel.GetNewKey(allenum, a => a.CATEGORYCODE, Key, Tar);
+            if (DefineSave.CATEGORYID.IsEmpty())
+            {
+                DefineSave.CATEGORYID = service.CommonService.NewINC("CATEGORY");
+            }
+            DefineSave.CATEGORYCODE = newkey;
+            DefineSave.LEVEL_LAST = "1";
+            CommonSave(DefineSave);
+            return newkey;
+        }
+    }
+}
