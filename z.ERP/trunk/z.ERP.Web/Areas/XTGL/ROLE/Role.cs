d5100af20f17fe492a8a50f8707b44a8fc4c8b9c
hp <hp@HP-PC>
2018-03-05 00:51:10 +0800

    调整角色

---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/ROLE/Role.cs
@@ -0,0 +1,38 @@
+﻿using z.ERP.Web.Areas.Base;
+using System.Web.Mvc;
+using z.ERP.Entities;
+using z.Extensions;
+using System;
+
+namespace z.ERP.Web.Areas.XTGL.ROLE
+{
+    public class RoleController: BaseController
+    {
+        public ActionResult Role()
+        {
+            ViewBag.Title = "用户信息";
+            return View();
+        }
+
+        public string Save(ROLEEntity DefineSave)
+        {
+            var v = GetVerify(DefineSave);
+            if (DefineSave.ROLEID.IsEmpty())
+            {
+                DefineSave.ROLEID = service.CommonService.NewINC("ROLE");
+            }
+            v.IsUnique(a => a.ROLEID);
+            v.IsUnique(a => a.ROLECODE);
+            v.Require(a => a.ROLENAME);
+            v.Require(a => a.ORGID);
+            v.Verify();
+            return CommonSave(DefineSave);
+        }
+
+        public void Delete(ROLEEntity DefineDelete)
+        {
+            var v = GetVerify(DefineDelete);
+            CommenDelete(DefineDelete);
+        }
+    }
+}
