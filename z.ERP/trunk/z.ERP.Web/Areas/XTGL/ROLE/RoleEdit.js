bbd41829a7bf5ec005a5e203da4ba1ead884c491
hp <hp@HP-PC>
2018-03-14 23:20:42 +0800

    修改role

--- z.ERP/trunk/z.ERP.Web/Areas/XTGL/ROLE/RoleEdit.js
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/ROLE/RoleEdit.js
@@ -1,29 +1,17 @@
 ﻿editDetail.beforeVue = function () {
-
-    editDetail.dataParam.colDef = [
-        {
-            title: "代码",
-            key: 'ROLECODE', width: 100
-        },
-        {
-            title: '名称',
-            key: 'ROLENAME', width: 150
-        }];
-    editDetail.dataParam.dataDef = [];
     editDetail.branchid = false;
     editDetail.service = "UserService";
     editDetail.method = "GetRoleElement";
     editDetail.methodList = "GetRole";
     editDetail.Key = "ROLEID";
-    editDetail.dataParam.ROLE_MENU = [];
-    if (!editDetail.dataParam.ROLE_FEE) {
-        editDetail.dataParam.ROLE_FEE = [{
+    editDetail.screenParam.ROLE_MENU = [];
+    if (!editDetail.screenParam.ROLE_FEE) {
+        editDetail.screenParam.ROLE_FEE = [{
             CHECKED: "false",
             TRIMID: "",
             NAME: "",
         }]
     }
-    editDetail.dataParam.value = "1";
 }
 
 editDetail.newRecord = function () {
@@ -36,20 +24,22 @@ editDetail.showOne = function (data, callback) {
         }, function (data) {
             $.extend(editDetail.dataParam, data.role);
             editDetail.dataParam.BILLID = data.role.ROLEID;
-            editDetail.dataParam.ROLE_MENU = data.menu.Obj;
-            editDetail.dataParam.ROLE_FEE = data.fee;
-            for (var i = 0; i < editDetail.dataParam.ROLE_FEE.length; i++) {
-                if (editDetail.dataParam.ROLE_FEE[i].DISABLED == 0) {
-                    editDetail.dataParam.ROLE_FEE[i].DISABLED = true;
+            //菜单
+            editDetail.screenParam.ROLE_MENU = data.menu.Obj;
+            //收费项目距
+            editDetail.screenParam.ROLE_FEE = data.fee;
+            for (var i = 0; i < editDetail.screenParam.ROLE_FEE.length; i++) {
+                if (editDetail.screenParam.ROLE_FEE[i].DISABLED == 0) {
+                    editDetail.screenParam.ROLE_FEE[i].DISABLED = true;
                 }
                 else {
-                    editDetail.dataParam.ROLE_FEE[i].DISABLED = false;
+                    editDetail.screenParam.ROLE_FEE[i].DISABLED = false;
                 }
-                if (editDetail.dataParam.ROLE_FEE[i].CHECKED == 0) {
-                    editDetail.dataParam.ROLE_FEE[i].CHECKED = false;
+                if (editDetail.screenParam.ROLE_FEE[i].CHECKED == 0) {
+                    editDetail.screenParam.ROLE_FEE[i].CHECKED = false;
                 }
                 else {
-                    editDetail.dataParam.ROLE_FEE[i].CHECKED = true;
+                    editDetail.screenParam.ROLE_FEE[i].CHECKED = true;
                 }
             }
             callback && callback();
@@ -57,7 +47,33 @@ editDetail.showOne = function (data, callback) {
 }
 
 editDetail.IsValidSave = function () {
-    //editDetail.dataParam.FEESUBJECT = this.$refs.selectData.getSelection();
-    var ss = editDetail.dataParam.ROLE_MENU;
+    editDetail.dataParam.ROLE_MENU = [];
+    editDetail.dataParam.ROLE_FEE = [];
+    function GetTreeMenuSelectedData(node) {
+        if (node.length > 0)
+        {
+            for (var i = 0; i < node.length; i++)
+            {
+                if(node[i].checked)
+                editDetail.dataParam.ROLE_MENU.push({ "MODULECODE": node[i].code,"MENUID":1 });
+                if (node[i].children.length > 0 && node[i].checked)
+                {
+                    GetTreeMenuSelectedData(node[i].children)
+                }
+            }
+        }
+    }
+
+    GetTreeMenuSelectedData(editDetail.screenParam.ROLE_MENU);    
+    for (var i = 0; i < editDetail.screenParam.ROLE_FEE.length; i++)
+    {
+        if (editDetail.screenParam.ROLE_FEE[i].CHECKED)
+        {
+            editDetail.dataParam.ROLE_FEE.push({ "TRIMID": editDetail.screenParam.ROLE_FEE[i].TRIMID });
+        }
+    }
     return true;
 }
+
+
+
