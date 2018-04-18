ab8017cb4cc880499c39c31deff001c142f50aa1
shizh <shizh@SZHPC>
2018-04-19 00:42:11 +0800

    添加商户收款处理

---
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/BILL_OBTAIN_SK/Bill_Obtain_SkController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using z.ERP.Entities;
+using z.ERP.Web.Areas.Base;
+
+namespace z.ERP.Web.Areas.JSGL.BILL_OBTAIN_SK
+{
+    public class Bill_Obtain_SkController: BaseController
+    {
+        public ActionResult Bill_Obtain_SkList()
+        {
+            ViewBag.Title = "商户收款处理";
+            return View();
+        }
+        public ActionResult Bill_Obtain_SkEdit(string Id)
+        {
+            ViewBag.Title = "编辑商户收款处理";
+            return View(model: Id);
+        }
+        public ActionResult Bill_Obtain_SkDetail(string Id)
+        {
+            ViewBag.Title = "浏览商户收款处理";
+            //var entity = service.JsglService.GetBillObtainSklDetail(new BILL_OBTAINEntity(Id));
+            //ViewBag.bill = entity.Item1;
+            //return View(entity);
+            return View();
+        }
+    }
+}
