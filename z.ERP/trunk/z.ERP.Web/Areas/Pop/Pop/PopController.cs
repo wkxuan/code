2a7a5380ce7a2832dfde716144b678457bb6d8d5
hp-PC\hp <xiyouwll@163.com>
2018-06-11 07:58:32 +0800

    no message

---
+++ z.ERP/trunk/z.ERP.Web/Areas/Pop/Pop/PopController.cs
@@ -0,0 +1,28 @@
+﻿using z.ERP.Web.Areas.Base;
+using System.Web.Mvc;
+using z.ERP.Entities;
+using z.Extensions;
+using System;
+using System.Collections.Generic;
+using z.MVC5.Results;
+using z.ERP.Model;
+using z.ERP.Entities.Enum;
+using System.Data;
+
+namespace z.ERP.Web.Areas.Pop.Pop
+{
+    public class PopController : BaseController
+    {
+        public ActionResult PopBillList()
+        {
+            ViewBag.Title = "账单";
+            return View();
+        }
+        public ActionResult PopFeeSubjectList()
+        {
+            ViewBag.Title = "收费项目";
+            return View();
+        }
+    }
+
+}
