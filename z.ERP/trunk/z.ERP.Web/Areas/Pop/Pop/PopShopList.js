41d47cd04809bba4227a50d686aaa07702cdadcb
hp-PC\hp <xiyouwll@163.com>
2018-06-19 13:59:57 +0800

    上传重命名文件

---
+++ z.ERP/trunk/z.ERP.Web/Areas/Pop/Pop/PopShopList.js
@@ -0,0 +1,16 @@
+﻿search.beforeVue = function () {
+    var col = [
+        { title: "分店名称", key: 'BRANCHNAME', width: 200 },
+        { title: '楼层名称', key: 'FLOORNAME', width: 200 },
+        { title: '单元代码', key: 'CODE', width: 100 },
+        { title: '单元名称', key: 'NAME', width: 150 }
+    ];
+    search.screenParam.colDef = col.concat(search.colMul);
+    search.service = "XtglService";
+    search.method = "GetShop";
+}
+//获取父页面参数
+search.popInitParam = function (data) {
+    search.searchParam.BRANCHID = data.BRANCHID;
+}
+
---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/FLOOR/FloorController.cs
@@ -0,0 +1,56 @@
+﻿using z.ERP.Web.Areas.Base;
+using System.Web.Mvc;
+using z.ERP.Entities;
+using z.Extensions;
+using System;
+using z.MVC5.Results;
+
+namespace z.ERP.Web.Areas.XTGL.FLOOR
+{
+    public class FloorController: BaseController
+    {
+        public ActionResult Floor()
+        {
+            ViewBag.Title = "楼层信息";
+            return View();
+        }
+
+        public string Save(FLOOREntity DefineSave)
+        {
+            var v = GetVerify(DefineSave);
+            if (DefineSave.ID.IsEmpty())
+            {
+                DefineSave.ID = service.CommonService.NewINC("FLOOR");
+            }
+            v.IsUnique(a => a.ID);
+            v.IsUnique(a => a.CODE);
+            v.Require(a => a.NAME);
+            v.Require(a => a.BRANCHID);
+            v.Require(a => a.ORGID);
+            v.Require(a => a.AREA_BUILD);
+            v.Require(a => a.STATUS);
+            v.Verify();
+            return CommonSave(DefineSave);
+        }
+
+        public void Delete(FLOOREntity DefineDelete)
+        {
+            var v = GetVerify(DefineDelete);
+            CommenDelete(DefineDelete);
+        }
+        public UIResult SearchInit()
+        {
+            var res = service.DataService.GetTreeOrg();
+            return new UIResult(
+                new
+                {
+                    treeOrg = res.Item1
+                }
+            );
+        }
+        public UIResult GetBranch(BRANCHEntity Data)
+        {
+            return new UIResult(service.DataService.GetBranch(Data));
+        }
+    }
+}
