92febb5fb167a34f3d5892dd549ca10a478f2fd4
王凯旋 <wang.kx@live.cn>
2018-07-03 16:39:07 +0800

    业态换控件

--- z.ERP/trunk/z.ERP.Web/Areas/PPGL/BRAND/BrandEdit.js
+++ z.ERP/trunk/z.ERP.Web/Areas/PPGL/BRAND/BrandEdit.js
@@ -18,3 +18,18 @@ editDetail.showOne = function (data, callback) {
         
     });
 }
+
+editDetail.mountedInit = function () {
+    _.Ajax('SearchInit', {
+        Data: {}
+    }, function (data) {
+        Vue.set(editDetail.screenParam, "CATEData", data.treeOrg.Obj);
+    });
+}
+
+
+editDetail.otherMethods = {
+    orgChange: function (value, selectedData) {
+        editDetail.dataParam.CATEGORYID = value[value.length - 1];  //有问题，不管选哪个节点，结果都是第一个节点的值3
+    },
+}
