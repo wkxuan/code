8a0ff55e59c2c5c3b436199a4c7a5f376c0a1d1a
王凯旋 <wang.kx@live.cn>
2018-07-18 10:01:04 +0800

    去掉多余引用

--- z.ERP/trunk/z.ERP.Services/XtglService.cs
+++ z.ERP/trunk/z.ERP.Services/XtglService.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Remoting;
-using System.Runtime.Remoting.Activation;
-using System.Runtime.Remoting.Contexts;
-using System.Runtime.Remoting.Messaging;
-using System.Runtime.Remoting.Proxies;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 using z.ERP.Entities;
 using z.ERP.Entities.Enum;
 using z.ERP.Model.Vue;
@@ -18,8 +9,6 @@ using z.Exceptions;
 using z.Extensions;
 using z.Extensiont;
 using z.MVC5.Results;
-using z.WebPage;
-using z.SSO.Model;
 
 namespace z.ERP.Services
 {
