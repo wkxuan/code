ecaf1532d52b8da22c651f16bc216879aeca59a4
王凯旋 <wang.kx@live.cn>
2018-09-12 17:44:52 +0800

    选择账单增加传递合同条件

--- z.ERP/trunk/z.ERP.Services/DataService.cs
+++ z.ERP/trunk/z.ERP.Services/DataService.cs
@@ -3,20 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Remoting;
-using System.Runtime.Remoting.Activation;
-using System.Runtime.Remoting.Contexts;
-using System.Runtime.Remoting.Messaging;
-using System.Runtime.Remoting.Proxies;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 using z.ERP.Entities;
 using z.Extensions;
 using z.MVC5.Results;
 using z.Results;
-using z.WebPage;
 using z.ERP.Entities.Enum;
 using z.ERP.Model.Vue;
 
@@ -202,6 +192,7 @@ namespace z.ERP.Services
             
             int count;
             DataTable dt = DbHelper.ExecuteTable(sql, item.PageInfo, out count);
+            dt.NewEnumColumns<结算单状态>("STATUS", "STATUSMC");
             return new DataGridResult(dt, count);
         }
 
