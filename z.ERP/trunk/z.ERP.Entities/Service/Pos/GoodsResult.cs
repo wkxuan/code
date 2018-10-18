0d710dc2bebea2640f44d38a0003373b6999e5a2
王凯旋 <wang.kx@live.cn>
2018-10-18 17:24:03 +0800

    增加按日期返回支付汇总和明细接口

--- z.ERP/trunk/z.ERP.Entities/Service/Pos/GoodsResult.cs
+++ z.ERP/trunk/z.ERP.Entities/Service/Pos/GoodsResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace z.ERP.Entities.Service.Pos
+﻿namespace z.ERP.Entities.Service.Pos
 {
     public class GoodsResult
     {
---
+++ z.ERP/trunk/z.ERP.Entities/Service/Pos/PayDetailResult.cs
@@ -0,0 +1,35 @@
+﻿namespace z.ERP.Entities.Service.Pos
+{
+    public class PayDetailResult
+    {
+        public string posno
+        {
+            get;
+            set;
+        }
+
+        public int dealid
+        {
+            get;
+            set;
+        }
+
+        public int payid
+        {
+            get;
+            set;
+        }
+
+        public string payname
+        {
+            get;
+            set;
+        }
+
+        public decimal amount
+        {
+            get;
+            set;
+        }
+    }
+}
