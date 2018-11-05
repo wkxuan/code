01086b51639260b99f6ad898b6c331297996a2c4
hp-PC\hp <xiyouwll@163.com>
2018-11-05 16:24:07 +0800

    添加凭证

---
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/VOUCHER/VoucherController.cs
@@ -0,0 +1,72 @@
+﻿using z.ERP.Web.Areas.Base;
+using System.Web.Mvc;
+using z.ERP.Entities;
+using System.Collections.Generic;
+using z.MVC5.Results;
+using z.ERP.Web.Areas.Layout.Search;
+using z.MVC5.Attributes;
+using z.ERP.Web.Areas.Layout.EditDetail;
+
+namespace z.ERP.Web.Areas.JSGL.VOUCHER
+{
+    public class VoucherController : BaseController
+    {
+        public ActionResult VoucherList()
+        {
+            ViewBag.Title = "凭证模板";
+            return View(new SearchRender()
+            {
+                Permission_Browse = "108001",
+                Permission_Add = "108001",
+                Permission_Del = "108001",
+                Permission_Edit = "108001",
+                Permission_Exec = "108001"
+            });
+        }
+        public ActionResult VoucherEdit(string Id)
+        {
+            ViewBag.Title = "凭证模板编辑";
+            return View("VoucherEdit", model: (EditRender)Id);
+        }
+        public ActionResult VoucherDetail(string Id)
+        {
+            ViewBag.Title = "凭证模板浏览";
+            var entity = service.CwglService.GetVoucherElement(new VOUCHEREntity(Id));
+            ViewBag.billAdjust = entity.Item1;
+            ViewBag.billAdjustItem = entity.Item2;
+            return View();  //entity
+        }
+
+        public void Delete(List<VOUCHEREntity> DeleteData)
+        {
+            service.CwglService.DeleteVoucher(DeleteData);
+        }
+
+        [Permission("108001")]
+        public string Save(VOUCHEREntity SaveData)
+        {
+            return service.CwglService.SaveVoucher(SaveData);
+        }
+
+        public UIResult SearchVoucher(VOUCHEREntity Data)
+        {
+            var res = service.CwglService.GetVoucherElement(Data);
+            return new UIResult(
+                new
+                {
+                    voucher= res.Item1,
+                    voucherSql = res.Item2,
+                    voucherRecord = res.Item3,
+                    voucherRecordPzkm = res.Item4,
+                    voucherRecordZy = res.Item5
+                }
+                );
+        }
+        [Permission("108001")]
+        public void ExecData(VOUCHEREntity Data)
+        {
+            service.CwglService.ExecVoucher(Data);
+        }
+
+    }
+}
---
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/VOUCHER/VoucherList.js
@@ -0,0 +1,40 @@
+﻿search.beforeVue = function () {
+    var col = [
+        { title: "编号", key: "VOUCHERID", width: 105, sortable: true },
+        { title: "名称", key: "VOUCHERNAME", width: 150,sortable: true},
+        //{ title: "类型", key: "VOUCHERTYPEMC", width: 80, sortable: true },
+        { title: "状态", key: "STATUSMC", width: 80 },
+        { title: "备注", key: "DISCRIPTION", width: 200 },
+        { title: "登记人", key: "REPORTER_NAME", width: 90 },
+        { title: "登记时间", key: "REPORTER_TIME", width: 150, sortable: true },
+        { title: "审核人", key: "VERIFY_NAME", width: 90 },
+        { title: "审核时间", key: "VERIFY_TIME", width: 150, sortable: true }
+    ];
+    search.screenParam.colDef = col.concat(search.colOperate).concat(search.colMul);
+    search.service = "CwglService";
+    search.method = "GetVoucherList";
+}
+
+search.browseHref = function (row, index) {
+    _.OpenPage({
+        id: 108001,
+        title: '浏览凭证模板',
+        url: "JSGL/VOUCHER/VoucherDetail/" + row.VOUCHERID
+    });
+}
+
+search.addHref = function (row) {
+    _.OpenPage({
+        id: 108001,
+        title: '新增凭证模板',
+        url: "JSGL/VOUCHER/VoucherEdit/"
+    });
+}
+search.modHref = function (row, index) {
+    _.OpenPage({
+        id: 108001,
+        title: '编辑凭证模板',
+        url: "JSGL/VOUCHER/VoucherEdit/" + row.VOUCHERID
+    });
+}
+
