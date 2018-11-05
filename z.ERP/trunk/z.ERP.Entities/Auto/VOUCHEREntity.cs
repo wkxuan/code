fcdf494d3e8504a0ea5b93e637923645210a0651
hp-PC\hp <xiyouwll@163.com>
2018-11-05 11:37:37 +0800

    修改凭证ENTITY

---
+++ z.ERP/trunk/z.ERP.Entities/Auto/VOUCHEREntity.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * 这是自动生成的代码文件，请勿做任何修改。
+ * 生成时间：2018/3/22 0:39:05
+ * 生成人：书房
+ * 代码生成器版本号：1.2.6655.1027
+ *
+ */ 
+
+using System.Data;
+using z.DbHelper.DbDomain;
+
+namespace z.ERP.Entities
+{
+    [DbTable("VOUCHER", "凭证模板")]
+    public partial class VOUCHEREntity : TableEntityBase
+    {
+        public VOUCHEREntity()
+        {
+        }
+
+        public VOUCHEREntity(string voucherid)
+        {
+            VOUCHERID = voucherid;
+        }
+
+        /// <summary>
+        /// 单号
+        /// <summary>
+        [PrimaryKey]
+        [Field("单号")]
+        public string VOUCHERID
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 名称
+        /// <summary>
+        [Field("名称")]
+        public string VOUCHERNAME
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 类型
+        /// <summary>
+        [Field("类型")]
+        public string VOUCHERTYPE
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 状态
+        /// <summary>
+        [Field("状态")]
+        public string STATUS
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 描述
+        /// <summary>
+        [Field("描述")]
+        public string DESCRIPTION
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 登记人
+        /// <summary>
+        [Field("登记人")]
+        public string REPORTER
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 登记人名称
+        /// <summary>
+        [Field("登记人名称")]
+        public string REPORTER_NAME
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 登记时间
+        /// <summary>
+        [Field("登记时间")]
+        [DbType(DbType.DateTime)]
+        public string REPORTER_TIME
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 审核人
+        /// <summary>
+        [Field("审核人")]
+        public string VERIFY
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 审核时间
+        /// <summary>
+        [Field("审核时间")]
+        public string VERIFY_NAME
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 审核日期
+        /// <summary>
+        [Field("审核日期")]
+        [DbType(DbType.DateTime)]
+        public string VERIFY_TIME
+        {
+            get; set;
+        }
+    }
+}
---
+++ z.ERP/trunk/z.ERP.Entities/VOUCHEREntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using z.DbHelper.DbDomain;
+
+namespace z.ERP.Entities
+{
+    public partial class VOUCHEREntity
+    {
+        [ForeignKey(nameof(VOUCHERID), nameof(VOUCHER_MAKESQLEntity.VOUCHERID))]
+        public List<VOUCHER_MAKESQLEntity> VOUCHER_MAKESQL
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey(nameof(VOUCHERID), nameof(VOUCHER_RECORDEntity.VOUCHERID))]
+        public List<VOUCHER_RECORDEntity> VOUCHER_RECORD
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey(nameof(VOUCHERID), nameof(VOUCHER_RECORD_PZKMEntity.VOUCHERID))]
+        public List<VOUCHER_RECORD_PZKMEntity> VOUCHER_RECORD_PZKM
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey(nameof(VOUCHERID), nameof(VOUCHER_RECORD_ZYEntity.VOUCHERID))]
+        public List<VOUCHER_RECORD_ZYEntity> VOUCHER_RECORD_ZY
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
