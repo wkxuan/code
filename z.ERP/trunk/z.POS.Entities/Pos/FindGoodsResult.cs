6398266a228a92e08e6327393458948799854111
王凯旋 <wang.kx@live.cn>
2019-02-27 16:22:41 +0800

    cypos接口完善

---
+++ z.ERP/trunk/z.POS.Entities/Pos/FindGoodsResult.cs
@@ -0,0 +1,41 @@
+﻿namespace z.POS.Entities.Pos
+{
+    public class FindGoodsResult
+    {
+        public int goodsid
+        {
+            get; set;
+        }
+
+        public string goodscode
+        {
+            get;
+            set;
+        }
+        public string name
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 类型：1品类2大类
+        /// </summary>
+        public int type
+        {
+            get; set;
+        }
+        public decimal price
+        {
+            get; set;
+        }
+        public decimal member_Price
+        {
+            get; set;
+        }
+
+        public int shopid
+        {
+            get; set;
+        }
+
+    }
+}
