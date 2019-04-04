89ddbe213de823041f3de585ff6a8aaf2f75664a
linaj <41703243@qq.com>
2019-04-04 17:13:53 +0800

    商户，品牌不提供浏览,审核,删除功能

--- z.ERP/trunk/z.ERP.Web/Areas/SHGL/MERCHANT/MerchantController.cs
+++ z.ERP/trunk/z.ERP.Web/Areas/SHGL/MERCHANT/MerchantController.cs
@@ -46,11 +46,11 @@ namespace z.ERP.Web.Areas.SHGL.MERCHANT
             ViewBag.Title = "商户列表信息";
             return View(new SearchRender()
             {
-                Permission_Browse = "10200100",
+                //Permission_Browse = "10200100",
                 Permission_Add = "10200101",
-                Permission_Del = "10200101",
-                Permission_Edit= "10200101",
-                Permission_Exec= "10200102"
+                // Permission_Del = "10200101",
+                Permission_Edit = "10200101",
+                //Permission_Exec = "10200102"
             });
         }
 
