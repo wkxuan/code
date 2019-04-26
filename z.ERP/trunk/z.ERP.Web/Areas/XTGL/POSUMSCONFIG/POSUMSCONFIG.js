0b3ecb2765f2e3a2d1bc7a00efaab9aa8b770763
董泽恺 <83956250@qq.com>
2019-04-26 15:16:18 +0800

    POS银联支付配置

---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/POSUMSCONFIG/POSUMSCONFIG.js
@@ -0,0 +1,57 @@
+﻿define.beforeVue = function () {
+
+    define.screenParam.colDef = [
+    { title: '终端号', key: 'POSNO', width: 400 }
+    ];
+
+    define.screenParam.dataDef = [];
+    define.service = "XtglService";
+    define.method = "GetPOSUMSCONFIG";
+    define.methodList = "GetPOSUMSCONFIG";
+    define.Key = 'POSNO';
+
+}
+
+define.IsValidSave = function () {
+    if (!define.dataParam.POSNO) {
+        iview.Message.info("终端号不能为空!");
+        return false;
+    }
+    if (!define.dataParam.IP) {
+        iview.Message.info("IP地址不能为空!");
+        return false;
+    }
+
+    if (!define.dataParam.IP_BAK) {
+        iview.Message.info("备用IP地址不能为空!");
+        return false;
+    }
+    if (!define.dataParam.PORT) {
+        iview.Message.info("端口号不能未空!");
+        return false;
+    }
+
+    if (!define.dataParam.CFX_MCHTID) {
+        iview.Message.info("银联实体卡商户号不能为空!");
+        return false;
+    }
+
+    if (!define.dataParam.CFX_TERMID) {
+        iview.Message.info("银联实体卡终端号不能为空!");
+        return false;
+    }
+    if (!define.dataParam.CFXMPAY_MCHTNAME) {
+        iview.Message.info("银行扫码商户名称不能为空!");
+        return false;
+    }
+
+    if (!define.dataParam.CFXMPAY_MCHTID) {
+        iview.Message.info("银行扫码商户号不能为空!");
+        return false;
+    }
+    if (!define.dataParam.CFXMPAY_TERMID) {
+        iview.Message.info("银行扫码终端号不能为空!");
+        return false;
+    }
+    return true;
+}
