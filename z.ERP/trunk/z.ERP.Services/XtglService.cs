972ee31cb9636c27012e205346f122b97c9013a1
wlsxjz <41703243@qq.com>
2019-05-06 11:12:52 +0800

    预警

--- z.ERP/trunk/z.ERP.Services/XtglService.cs
+++ z.ERP/trunk/z.ERP.Services/XtglService.cs
@@ -525,10 +525,68 @@ namespace z.ERP.Services
             return new DataGridResult(dt, count);
         }
 
+
+
+        public DataGridResult GetAlert(SearchItem item)
+        {
+
+            string sql = $@"SELECT ID,MC,XSSX,SQLSTR FROM DEF_ALERT WHERE 1=1 ";
+            item.HasKey("ID", a => sql += $" and ID LIKE '%{a}%'");
+            sql += " order by ID";
+            int count;
+            DataTable dt = DbHelper.ExecuteTable(sql, item.PageInfo, out count);
+            return new DataGridResult(dt, count);
+        }
+
         public SHOPEntity SelectShop(string shop)
         {
             return DbHelper.Select(new SHOPEntity() { SHOPID = shop });
         }
+
+        public string SaveAlert(DEF_ALERTEntity DefineSave)
+        {
+            var v = GetVerify(DefineSave);
+            v.Require(a => a.MC);
+            v.Require(a => a.SQLSTR);
+
+            var b = false;
+            b = VerifySql(DefineSave.SQLSTR);
+            if (b == false)
+            {
+                throw new LogicException("SQL语句不正确!");
+            };
+            v.IsUnique(a => a.MC);
+            v.Verify();
+            if (DefineSave.ID.IsEmpty())
+                DefineSave.ID = CommonService.NewINC("DEF_ALERT");
+            CommonService.CommonSave(DefineSave);
+            return DefineSave.ID;
+        }
+
+        public bool VerifySql(string sql)
+        {
+            var b = false;
+            try
+            {
+                DataTable dt = DbHelper.ExecuteTable(sql);
+                b = true;
+            }
+            catch
+            {
+                b = false;
+            }
+            return b;
+        }
+
+
+        public Tuple<DataTable> GetAlertSql(DEF_ALERTEntity Data)
+        {
+            DEF_ALERTEntity alert = new DEF_ALERTEntity();
+            alert = DbHelper.Select(new DEF_ALERTEntity() { ID = Data.ID });
+            DataTable alertSql = DbHelper.ExecuteTable(alert.SQLSTR);
+            return new Tuple<DataTable>(alertSql);
+        }
+
     }
 
 }
