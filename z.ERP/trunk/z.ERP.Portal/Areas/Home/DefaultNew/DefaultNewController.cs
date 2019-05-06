bc8b0d7afe68bdf9cb36c0cef4edee8ef7958720
董泽恺 <83956250@qq.com>
2019-05-06 14:45:13 +0800

    报表切换   重置TABLE 及分页参数

---
+++ z.ERP/trunk/z.ERP.Portal/Areas/Home/DefaultNew/DefaultNewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using z.ERP.Web.Areas.Base;
+
+namespace z.ERP.Portal.Areas.Home.DefaultNew
+{
+    public class DefaultNewController:BaseController
+    {
+        public ActionResult DefaultNew() {
+            return View();
+        }
+    }
+}
