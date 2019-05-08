520ad7e789a195aafbac7a7db93b904182da5fc9
董泽恺 <83956250@qq.com>
2019-05-08 15:11:09 +0800

    首页更新

---
+++ z.ERP/trunk/z.ERP.Portal/Areas/Home/DefaultNew/DefaultNew.js
@@ -0,0 +1,58 @@
+﻿var DefaultNew = new Vue({
+    el: '#main',
+    data: {
+        box1colDef: [
+                { title: ' ', key: 'TIME', width: 100, },
+                { title: '营业额(万元)', key: 'AMOUNT', width: 120, },
+                { title: '同比(%)', key: 'T_BI', width: 100, },
+                { title: '环比(%)', key: 'H_BI', width: 100, }, ],
+        box1dataDef: [],
+        box2colDef: [
+                { title: ' ', key: 'TYPE', width: 100, },
+                { title: '店铺数量(个)', key: 'NUMBERS', width: 130, },
+                { title: '租赁面积(m²)', key: 'AREA', width: 130, }, ],
+        box2dataDef: [],
+        box3colDef: [
+                { title: ' ', key: 'NO', width: 80, },
+                { title: '商铺名称', key: 'SHOPNAME', width: 200, },
+                { title: '租赁面积(m²)', key: 'AREA', width: 150, },
+                { title: '营业额(元)', key: 'AMOUNT', width: 150, }, ],
+        box3dataDef: [],
+        box6colDef: [
+                { title: ' ', key: 'NO', width: 80, },
+                { title: '业态名称', key: 'AREANAME', width: 200, },
+                { title: '租赁面积(m²)', key: 'AREA', width: 150, },
+                { title: '营业额(元)', key: 'AMOUNT', width: 150, }, ],
+        box6dataDef: [],
+        tableH: 300
+    },
+    mounted: function () {
+        var type = '1';   //初始化默认昨日数据
+        _.Ajax('BoxData', {
+            type:type
+        }, function (data) {
+            DefaultNew.box2dataDef = data.box2data;
+            DefaultNew.box3dataDef = data.box3data;
+            DefaultNew.box6dataDef = data.box6data;
+        });
+    },
+    methods: {
+        //根据日期筛选数据
+        box3datechange: function (event) {
+            var type = event;
+            _.Ajax('Box3Data', {
+                type: type
+            }, function (data) {
+                DefaultNew.box3dataDef = data;
+            });
+        },
+        box6datechange: function (event) {
+            var type = event;
+            _.Ajax('Box6Data', {
+                type: type
+            }, function (data) {
+                DefaultNew.box6dataDef = data;
+            });
+        }
+    }
+})
---
+++ z.ERP/trunk/z.ERP.Services/DefaultDataService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using z.MVC5.Results;
+
+namespace z.ERP.Services
+{
+    public class DefaultDataService: ServiceBase
+    {
+        internal DefaultDataService()
+        {
+        }
+        /// <summary>
+        /// 模块1数据
+        /// </summary>
+        /// <returns></returns>
+        public DataTable Box1Data() {
+            string sql = @"";
+
+            DataTable dt = DbHelper.ExecuteTable(sql);
+            return dt;
+        }
+        /// <summary>
+        /// 模块2数据
+        /// </summary>
+        /// <returns></returns>
+        public DataTable Box2Data()
+        {
+            string sql = @"SELECT (CASE RENT_STATUS  WHEN 1 THEN '正在经营' WHEN 2 THEN '闲置招租' end)TYPE,COUNT(1) NUMBERS,SUM(A.AREA_RENTABLE) AREA
+                              FROM SHOP A
+                              GROUP BY RENT_STATUS";
+
+            DataTable dt = DbHelper.ExecuteTable(sql);
+            return dt;
+        }
+        /// <summary>
+        /// 模块3数据
+        /// </summary>
+        /// <param name="type">数据日期 1.昨日2.本月3.上月</param>
+        /// <returns></returns>
+        public DataTable Box3Data(string type)
+        {
+            string sql = "";
+            if (type == "1")
+            {
+                sql = @"SELECT * FROM (
+                            select row_number() over(order by SUM(AMOUNT) desc) NO,B.NAME SHOPNAME,B.AREA_RENTABLE AREA,SUM(AMOUNT) AMOUNT
+                             from CONTRACT_SUMMARY A,SHOP B
+                             WHERE A.SHOPID=B.SHOPID AND TO_CHAR(RQ,'yyyy-MM-dd')=TO_CHAR(SYSDATE-1,'yyyy-MM-dd')
+                             GROUP BY B.NAME,B.AREA_RENTABLE) Z
+                             WHERE ROWNUM <=10";
+            }
+            else if (type == "2")
+            {
+                sql = @"SELECT * FROM (
+                            select row_number() over(order by SUM(AMOUNT) desc) NO,B.NAME SHOPNAME,B.AREA_RENTABLE AREA,SUM(AMOUNT) AMOUNT
+                             from CONTRACT_SUMMARY A,SHOP B
+                             WHERE A.SHOPID=B.SHOPID AND TO_CHAR(RQ,'yyyy-MM')=TO_CHAR(SYSDATE,'yyyy-MM')
+                             GROUP BY B.NAME,B.AREA_RENTABLE) Z
+                             WHERE ROWNUM <=10";
+            }
+            else if (type == "3")
+            {    //
+                sql = @"SELECT* FROM (select row_number() over(order by SUM(AMOUNT) desc) NO, B.NAME SHOPNAME, B.AREA_RENTABLE AREA, SUM(AMOUNT) AMOUNT
+                             from CONTRACT_SUMMARY A, SHOP B
+                             WHERE A.SHOPID = B.SHOPID AND TO_CHAR(RQ, 'yyyy-MM') = TO_CHAR(ADD_MONTHS(SYSDATE, -1), 'yyyy-MM')
+                             GROUP BY B.NAME, B.AREA_RENTABLE) Z
+                              WHERE ROWNUM <= 10";
+            }
+            DataTable dt = DbHelper.ExecuteTable(sql);
+            return dt;
+        }
+        /// <summary>
+        /// 模块6数据
+        /// </summary>
+        /// <param name="type">数据日期 1.昨日2.本月3.上月</param>
+        /// <returns></returns>
+        public DataTable Box6Data(string type)
+        {
+            string sql = "";
+            if (type == "1") {
+                sql = @"SELECT * FROM (
+                        select row_number() over(order by SUM(AMOUNT) desc) NO,C.CATEGORYNAME AREANAME,SUM(B.AREA_RENTABLE) AREA,SUM(AMOUNT) AMOUNT
+                         from CONTRACT_SUMMARY A,SHOP B,CATEGORY C
+                         WHERE A.SHOPID=B.SHOPID AND B.CATEGORYID=C.CATEGORYID AND TO_CHAR(RQ,'yyyy-MM-dd')=TO_CHAR(SYSDATE-1,'yyyy-MM-dd')
+                         GROUP BY C.CATEGORYNAME) Z
+                         WHERE ROWNUM <=10";
+            }
+            else if (type == "2")
+            {
+                sql = @"SELECT * FROM (
+                        select row_number() over(order by SUM(AMOUNT) desc) NO,C.CATEGORYNAME AREANAME,SUM(B.AREA_RENTABLE) AREA,SUM(AMOUNT) AMOUNT
+                         from CONTRACT_SUMMARY A,SHOP B,CATEGORY C
+                         WHERE A.SHOPID=B.SHOPID AND B.CATEGORYID=C.CATEGORYID AND TO_CHAR(RQ,'yyyy-MM')=TO_CHAR(SYSDATE,'yyyy-MM')
+                         GROUP BY C.CATEGORYNAME) Z
+                         WHERE ROWNUM <=10";
+            }
+            else if (type == "3")
+            {
+                sql = @"SELECT * FROM (
+                        select row_number() over(order by SUM(AMOUNT) desc) NO,C.CATEGORYNAME AREANAME,SUM(B.AREA_RENTABLE) AREA,SUM(AMOUNT) AMOUNT
+                         from CONTRACT_SUMMARY A,SHOP B,CATEGORY C
+                         WHERE A.SHOPID=B.SHOPID AND B.CATEGORYID=C.CATEGORYID AND TO_CHAR(RQ,'yyyy-MM')=TO_CHAR(ADD_MONTHS(SYSDATE, -1),'yyyy-MM')
+                         GROUP BY C.CATEGORYNAME) Z
+                         WHERE ROWNUM <=10";
+            }
+            DataTable dt = DbHelper.ExecuteTable(sql);
+            return dt;
+        }
+    }
+}
