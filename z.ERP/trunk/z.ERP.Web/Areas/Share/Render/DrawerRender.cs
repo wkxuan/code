2d3bb3f9f0d3f472e42540f013507114d28c5d50
wlsxjz <41703243@qq.com>
2019-05-19 17:09:07 +0800

    审批流程定义，封装右侧栏控件待优化

---
+++ z.ERP/trunk/z.ERP.Web/Areas/Share/Render/DrawerRender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using z.MVC5.Models;
+
+namespace z.ERP.Web.Areas.Share.Render
+{
+    public class DrawerRender : VueRender
+    {
+        public override string ControllerMothod
+        {
+            get
+            {
+                return "Drawer";
+            }
+        }
+        public string Title { get; set; }
+        public string Width { get; set; }
+        public string Styles { get; set; }
+        public string Closable { get; set; }
+        public string Draggable { get; set; }
+        public string Html { get; set; }
+    }
+}
---
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/SPLC/SPLCController.cs
@@ -0,0 +1,26 @@
+﻿using z.ERP.Web.Areas.Base;
+using System.Web.Mvc;
+using z.ERP.Entities;
+using z.MVC5.Results;
+using z.ERP.Web.Areas.Layout.Define;
+
+namespace z.ERP.Web.Areas.XTGL.SPLC
+{
+    public class SPLCController : BaseController
+    {
+        public ActionResult SPLC()
+        {
+
+            ViewBag.Title = "审批流程定义";
+            return View();
+        }
+
+        public string Save(DEF_ALERTEntity DefineSave)
+        {
+            return null;
+        }
+        public void Delete(DEF_ALERTEntity DefineDelete)
+        {
+        }
+    }
+}
