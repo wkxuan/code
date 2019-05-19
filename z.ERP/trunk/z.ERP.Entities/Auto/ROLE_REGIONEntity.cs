2ff9889ef57983200c5d77d48a769e96eb14ef13
shizh <shizh@SZHPC>
2019-05-19 23:03:08 +0800

    增加区域业态权限定义

---
+++ z.ERP/trunk/z.ERP.Entities/Auto/ROLE_REGIONEntity.cs
@@ -0,0 +1,37 @@
+﻿using z.DBHelper.DBDomain;
+
+namespace z.ERP.Entities
+{
+    [DbTable("ROLE_REGION", "角色-区域权限")]
+    public partial class ROLE_REGIONEntity : TableEntityBase
+    {
+        public ROLE_REGIONEntity()
+        {
+        }
+
+        public ROLE_REGIONEntity(string roleid, string regionid)
+        {
+            ROLEID = roleid;
+            REGIONID = regionid;
+        }
+
+        /// <summary>
+        /// 角色编码
+        /// <summary>
+        [PrimaryKey]
+        [Field("角色编码")]
+        public string ROLEID
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 区域ID
+        /// <summary>
+        [PrimaryKey]
+        [Field("区域ID")]
+        public string REGIONID
+        {
+            get; set;
+        }
+    }
+}
---
+++ z.ERP/trunk/z.ERP.Entities/Auto/ROLE_YTEntity.cs
@@ -0,0 +1,37 @@
+﻿using z.DBHelper.DBDomain;
+
+namespace z.ERP.Entities
+{
+    [DbTable("ROLE_YT", "角色-业态权限")]
+    public partial class ROLE_YTEntity : TableEntityBase
+    {
+        public ROLE_YTEntity()
+        {
+        }
+
+        public ROLE_YTEntity(string roleid, string ytid)
+        {
+            ROLEID = roleid;
+            YTID = ytid;
+        }
+
+        /// <summary>
+        /// 角色编码
+        /// <summary>
+        [PrimaryKey]
+        [Field("角色编码")]
+        public string ROLEID
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 业态ID
+        /// <summary>
+        [PrimaryKey]
+        [Field("业态ID")]
+        public string YTID
+        {
+            get; set;
+        }
+    }
+}
