e1845c5286f6c74f0339a444ec0777bb92f81a22
董泽恺 <83956250@qq.com>
2019-05-20 12:20:35 +0800

    发票popList

---
+++ z.ERP/trunk/z.ERP.Web/Areas/Pop/Pop/PopInvoiceList.js
@@ -0,0 +1,27 @@
+﻿search.beforeVue = function () {
+    var col = [
+        { title: "商户", key: "MERCHANTNAME", width: 200, sortable: true },
+        { title: "发票号码", key: "INVOICENUMBER", width: 100, sortable: true },
+        { title: "发票类型", key: "TYPENAME", width: 100 },
+        {
+            title: "开票日期", key: "INVOICEDATE", width: 115, sortable: true,
+            render: function (h, params) {
+                return h('div',
+                    this.row.INVOICEDATE.substr(0, 10));
+            }
+        },
+        { title: "不含税金额", key: "NOVATAMOUNT", width: 100 },
+        { title: "增值税金额", key: "VATAMOUNT", width: 100 },
+        { title: "发票金额", key: "INVOICEAMOUNT", width: 100 },
+        { title: "创建人", key: "CREATENAME", width: 150 },
+        { title: "创建时间", key: "CREATEDATE", width: 150 }
+    ];
+    search.screenParam.colDef = col.concat(search.colMul);
+    search.service = "JsglService";
+    search.method = "GetInvoiceList";
+}
+search.popInitParam = function (data) {
+    if (data) {
+        search.searchParam.MERCHANTID = data.MERCHANTID;
+    }
+}
