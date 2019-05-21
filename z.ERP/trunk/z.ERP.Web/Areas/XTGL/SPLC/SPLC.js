79a4bd4b7ab66f64a0c5bc88412393bed3effefe
wlsxjz <41703243@qq.com>
2019-05-22 00:10:58 +0800

    审批流程节点判断完成，待完善节点结果,做成左右过滤的模式

--- z.ERP/trunk/z.ERP.Web/Areas/XTGL/SPLC/SPLC.js
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/SPLC/SPLC.js
@@ -4,10 +4,11 @@
         spjdDrawer: false,
         spjdjgDrawer: false,
         colSplcjd: [
+            { type: 'selection', width: 60, align: 'center' },
             { title: '节点名称', key: 'JDNAME', width: 200 },
             { title: '类型', key: 'JDTYPE', width: 80 },
             { title: '角色组', key: 'ROLENAME', width: 100 },
-            { title: '顺序', key: 'JDINX', width: 80 }
+            { title: '顺序', key: 'JDINX', width: 80, sortable: true }
         ],
         SPLCJD: [],
 
@@ -47,6 +48,53 @@
             this.spjdDrawer = true;
         },
         sureSpjd: function () {
+
+            //判断节点数据合法性
+            //开始节点只能有一个,结束节点只能有一个
+            if (!this.JDNAME) {
+                iview.Message.info("请维护节点名称!");
+                return false;
+            };
+            if (!this.JDTYPE) {
+                iview.Message.info("请维护节点类型!");
+                return false;
+            };
+            if (!this.ROLENAME) {
+                iview.Message.info("请维护节点角色!");
+                return false;
+            };
+            if (!this.JDINX) {
+                iview.Message.info("请维护节点顺序!");
+                return false;
+            };
+            if (this.SPLCJD.length > 0) {
+                for (var i = 0; i < this.SPLCJD.length; i++) {
+                    if (this.JDTYPE == 1) {
+                        if (this.SPLCJD[i].JDTYPE == 1) {
+                            iview.Message.info("一个审批流程只能有一个开始节点!");
+                            return;
+                        };
+                    };
+                    if (this.JDTYPE == 3) {
+                        if (this.SPLCJD[i].JDTYPE == 3) {
+                            iview.Message.info("一个审批流程只能有一个结束节点!");
+                            return;
+                        };
+                    };
+                    if (this.SPLCJD[i].JDINX == this.JDINX) {
+                        iview.Message.info("当前的节点顺序已经存在!");
+                        return;
+                    };
+                };
+            };
+            let jdDataOne = {
+                JDNAME: this.JDNAME,
+                JDTYPE: this.JDTYPE,
+                ROLENAME: this.ROLENAME,
+                JDINX: this.JDINX,
+                ROLEID: this.ROLEID
+            };
+            this.SPLCJD.push(jdDataOne);
             this.spjdDrawer = false;
         },
         cancelSpjd: function () {
@@ -55,6 +103,12 @@
         SpjdJg: function () {
             if (this.SPLCJD.length == 0) {
                 iview.Message.info("请先定义审批流程节点!");
+                return;
+            };
+            //并且选中一条流程节点数据
+            var selectSolcjd = this.$refs.selectSplcjd.getSelection();
+            if (selectSolcjd.length != 1) {
+                iview.Message.info("请选中一条流程节点!");
                 return;
             };
             this.spjdjgDrawer = true;
