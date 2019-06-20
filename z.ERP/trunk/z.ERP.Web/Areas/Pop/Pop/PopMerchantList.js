13845331c0d7bd5b8f5966119d5abe52cd408cd6
刘银桥 <@DESKTOP-0QFCLD2>
2019-06-20 15:01:59 +0800

    弹窗、按钮样式修改

--- z.ERP/trunk/z.ERP.Web/Areas/Pop/Pop/PopMerchantList.js
+++ z.ERP/trunk/z.ERP.Web/Areas/Pop/Pop/PopMerchantList.js
@@ -1,10 +1,10 @@
 ﻿search.beforeVue = function () {
     var col = [
-        { title: "商户编号", key: "MERCHANTID", width: 100 },
-        { title: "商户名称", key: "NAME", width: 200 },
-        { title: "状态", key: "STATUSMC", width: 80 },
-        { title: "登记人", key: "REPORTER_NAME", width: 100 },
-        { title: "登记时间", key: "REPORTER_TIME", width: 150 }
+        { title: "商户编号", key: "MERCHANTID" },
+        { title: "商户名称", key: "NAME" },
+        { title: "状态", key: "STATUSMC" },
+        { title: "登记人", key: "REPORTER_NAME" },
+        { title: "登记时间", key: "REPORTER_TIME" }
     ];
     search.screenParam.colDef = col.concat(search.colMul);
     search.service = "ShglService";
