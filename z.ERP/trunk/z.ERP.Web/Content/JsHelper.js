16f17c2f972dc4eeb3ca544f1057827f1dec7967
刘银桥 <@DESKTOP-0QFCLD2>
2019-06-24 15:15:59 +0800

    添加yx-input组件

--- z.ERP/trunk/z.ERP.Web/Content/JsHelper.js
+++ z.ERP/trunk/z.ERP.Web/Content/JsHelper.js
@@ -103,5 +103,12 @@
                 fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
         return fmt;
     };
+    //生成全局唯一标识符
+    Guid = function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
 })(window);
 
