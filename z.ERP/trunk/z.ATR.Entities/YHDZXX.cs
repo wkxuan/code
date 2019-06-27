2186df3db834529d3fbdfb410af3331b6d8038f0
王凯旋 <wang.kx@live.cn>
2019-06-27 17:12:39 +0800

    增加  ATR  写数据库对应表操作

---
+++ z.ERP/trunk/z.ATR.Entities/YHDZXX.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using z.DBHelper.DBDomain;
+
+namespace z.ATR.Entities
+{
+
+    [DbTable("YHDZXX", "")]
+    public class YHDZXX : TableEntityBase
+    {
+        public string SHBM
+        {
+            get; set;
+        }
+
+        public string ZDBM
+        {
+            get; set;
+        }
+
+        public string ZDLSH
+        {
+            get; set;
+        }
+
+        public string JYLX
+        {
+            get; set;
+        }
+        public string JYCD
+        {
+            get; set;
+        }
+        public string DDBH
+        {
+            get; set;
+        }
+        public string JYRQ
+        {
+            get; set;
+        }
+        public string JYSJ
+        {
+            get; set;
+        }
+
+        public string JYJE
+        {
+            get; set;
+        }
+
+        public string YHKH
+        {
+            get; set;
+        }
+
+        public string JSZH
+        {
+            get; set;
+        }
+
+        public string SXF
+        {
+            get; set;
+        }
+
+    }
+}
--- z.ERP/trunk/z.ATR.Services/AutoService.cs
+++ z.ERP/trunk/z.ATR.Services/AutoService.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using z.ATR.Entities;
 using z.WSTools.Txt;
+using z.Extensions;
 
 namespace z.ATR.Services
 {
@@ -26,8 +27,9 @@ namespace z.ATR.Services
                 }
             });
             Log.Info("list", list);
+            
 
-            DataTable dt= TxtReader.ReadToDatatable(new TableReaderSettings()
+            DataTable dt = TxtReader.ReadToDatatable(new TableReaderSettings()
             {
                 FilePath = $@"C:\Users\zgy\Desktop\新建文件夹\ttt.txt",
                 ColumnSplit = new string[] { "|" },
@@ -39,12 +41,49 @@ namespace z.ATR.Services
                     { 3,"c" }
                 }
             });
-            Log.Info("DataTable", list);
+            Log.Info("DataTable", dt);
             //using (var tran = DbHelper.BeginTransaction())
             //{
             //    list.ForEach(l => DbHelper.Insert(l));
             //    tran.Commit();
             //}
+        }
+
+
+        public void insertYHDZXX()
+        {
+            string filePath  = ConfigExtension.GetConfig("FilePath").ToString();
+
+            List<YHDZXX> list = TxtReader.ReadToModel<YHDZXX>(new TableReaderSettings()
+            {
+                FilePath = filePath,
+                ColumnSplit = new string[] { "|" },
+                RowSplit = new string[] { "\r\n" },
+                RowSettings = new Dictionary<int, string>()
+                {
+                    { 1,"SHBM" },
+                    { 2,"ZDBM" },
+                    { 3,"ZDLSH" },
+                    { 4,"JYLX" },
+                    { 5,"JYCD" },
+                    { 6,"DDBH" },
+                    { 7,"JYRQ"},
+                    { 8,"JYSJ" },
+                    { 9,"JYJE" },
+                    { 10,"YHKH" },
+                    { 11,"JSZH" },
+                    { 12,"SXF" }
+                }
+            });
+
+            Log.Info("DataTable", list);
+
+
+            using (var tran = DbHelper.BeginTransaction())
+            {
+                list.ForEach(l => DbHelper.Insert(l));
+                tran.Commit();
+            }
         }
     }
 }
