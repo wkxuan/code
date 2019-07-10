76676baa9722719ee35300c9c790e048a6931272
董泽恺 <83956250@qq.com>
2019-07-10 15:43:37 +0800

    预收款，费用调整单

--- z.ERP/trunk/z.ERP.Web/Areas/JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskEdit.js
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskEdit.js
@@ -1,6 +1,7 @@
 ﻿editDetail.beforeVue = function () {
     editDetail.others = false;
     editDetail.branchid = true;
+    editDetail.otherPanel = false;
     editDetail.Key = 'BILLID';
     //预收款收款
     editDetail.dataParam.TYPE = 1;
@@ -35,3 +36,67 @@ editDetail.popCallBack = function (data) {
         }
     }
 };
+editDetail.mountedInit = function () {
+    editDetail.btnConfig = [{
+        id: "add",
+        authority: "10700401"
+    }, {
+        id: "edit",
+        authority: "10700401"
+    }, {
+        id: "del",
+        authority: "10700401"
+    }, {
+        id: "save",
+        authority: "10700401"
+    }, {
+        id: "abandon",
+        authority: "10700401"
+    }, {
+        id: "confirm",
+        name: "审核",
+        icon: "md-star",
+        authority: "10700402",
+        fun: function () {
+            _.Ajax('ExecData', {
+                Data: { BILLID: editDetail.dataParam.BILLID },
+            }, function (data) {
+                iview.Message.info("审核成功");
+                setTimeout(function () {
+                    window.location.reload();
+                }, 100);
+            });
+        },
+        enabled: function (disabled, data) {
+            if (!disabled && data.STATUS < 2) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        isNewAdd: true
+    }];
+};
+editDetail.IsValidSave = function () {
+    if (!editDetail.dataParam.BRANCHID) {
+        iview.Message.info("请选择分店!");
+        return false;
+    };
+    if (!editDetail.dataParam.MERCHANTID) {
+        iview.Message.info("请选择商户!");
+        return false;
+    };
+    if (!editDetail.dataParam.FKFSID) {
+        iview.Message.info("请选择付款方式!");
+        return false;
+    };
+    if (!editDetail.dataParam.FEE_ACCOUNT_ID) {
+        iview.Message.info("请选收费单位!");
+        return false;
+    };
+    if (!editDetail.dataParam.NIANYUE) {
+        iview.Message.info("请确认债券发生月!");
+        return false;
+    };
+    return true;
+}
--- z.ERP/trunk/z.ERP.Web/Areas/JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskList.js
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskList.js
@@ -1,5 +1,5 @@
 ﻿search.beforeVue = function () {
-    var col = [
+    search.screenParam.colDef = [
         { title: "单据编号", key: "BILLID", width: 105, sortable: true },
         { title: "商户代码", key: "MERCHANTID", width: 105, sortable: true },
         { title: "商户名称", key: "MERCHANTNAME", width: 200 },
@@ -10,34 +10,72 @@
         { title: "登记人", key: "REPORTER_NAME", width: 90 },
         { title: "登记时间", key: "REPORTER_TIME", width: 150 },
         { title: "审核人", key: "VERIFY_NAME", width: 90 },
-        { title: "审核时间", key: "VERIFY_TIME", width: 150 }
+        { title: "审核时间", key: "VERIFY_TIME", width: 150 },
+        {
+            title: '操作', key: 'operate', onClick: function (index, row, data) {
+                _.OpenPage({
+                    id: 10700401,
+                    title: '预收款收取单',
+                    url: "JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskEdit/" + row.BILLID
+                });
+            }
+        }
     ];
-    search.screenParam.colDef = col.concat(search.colOperate).concat(search.colMul);
     search.service = "JsglService";
     search.method = "GetBillObtainList";
     //预收款收款
     search.searchParam.TYPE = 1;
-}
-
-search.browseHref = function (row, index) {
-    _.OpenPage({
-        id: 107004,
-        title: '浏览预收款收取单',
-        url: "JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskDetail/" + row.BILLID
-    });
+    search.screenParam.showPopMerchant = false;
+    search.screenParam.srcPopMerchant = __BaseUrl + "/" + "Pop/Pop/PopMerchantList/";
+    search.screenParam.showPopSysuser = false;
+    search.screenParam.srcPopSysuser = __BaseUrl + "/" + "Pop/Pop/PopSysuserList/";
 }
 
 search.addHref = function (row) {
     _.OpenPage({
-        id: 107004,
+        id: 10700401,
         title: '新增预收款收取单',
         url: "JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskEdit/"
     });
 }
-search.modHref = function (row, index) {
-    _.OpenPage({
-        id: 107004,
-        title: '编辑预收款收取单',
-        url: "JSGL/BILL_OBTAIN_YSK/Bill_Obtain_YskEdit/" + row.BILLID
-    });
+search.otherMethods = {
+    SelMerchant: function () {
+        if (!search.searchParam.BRANCHID) {
+            iview.Message.info("请选择分店!");
+            return;
+        };
+        search.screenParam.showPopMerchant = true;
+    },
+    SelSysuser: function () {
+        search.screenParam.showPopSysuser = true;
+        btnFlag = "REPORTER";
+    },
+    SelSysuser_sh: function () {
+        search.screenParam.showPopSysuser = true;
+        btnFlag = "VERIFY";
+    },
 }
+//接收子页面返回值
+search.popCallBack = function (data) {
+    if (search.screenParam.showPopMerchant) {
+        search.screenParam.showPopMerchant = false;
+        for (var i = 0; i < data.sj.length; i++) {
+            search.searchParam.MERCHANTID = data.sj[i].MERCHANTID;
+            search.searchParam.MERCHANTNAME = data.sj[i].NAME;
+        }
+    }
+    if (search.screenParam.showPopSysuser) {
+        search.screenParam.showPopSysuser = false;
+        for (var i = 0; i < data.sj.length; i++) {
+            if (btnFlag == "REPORTER") {
+                search.searchParam.REPORTER = data.sj[i].USERID;
+                search.searchParam.REPORTER_NAME = data.sj[i].USERNAME;
+            }
+            else if (btnFlag == "VERIFY") {
+                search.searchParam.VERIFY = data.sj[i].USERID;
+                search.searchParam.VERIFY_NAME = data.sj[i].USERNAME;
+            }
+
+        };
+    }
+};
