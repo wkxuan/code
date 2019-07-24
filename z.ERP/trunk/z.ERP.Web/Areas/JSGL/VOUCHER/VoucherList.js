f2d051baf0af3546440b77ddc265fa6d466791bf
董泽恺 <83956250@qq.com>
2019-07-24 11:31:54 +0800

    table问题

--- z.ERP/trunk/z.ERP.Web/Areas/JSGL/VOUCHER/VoucherList.js
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/VOUCHER/VoucherList.js
@@ -1,40 +1,28 @@
 ﻿search.beforeVue = function () {
-    var col = [
+    search.screenParam.colDef = [
         { title: "编号", key: "VOUCHERID", width: 105, sortable: true },
         { title: "名称", key: "VOUCHERNAME", width: 150,sortable: true},
-        //{ title: "类型", key: "VOUCHERTYPEMC", width: 80, sortable: true },
-        //{ title: "状态", key: "STATUSMC", width: 80 },
         { title: "备注", key: "DISCRIPTION", width: 250 },
         { title: "登记人", key: "REPORTER_NAME", width: 90 },
         { title: "登记时间", key: "REPORTER_TIME", width: 150, sortable: true },
-        //{ title: "审核人", key: "VERIFY_NAME", width: 90 },
-        //{ title: "审核时间", key: "VERIFY_TIME", width: 150, sortable: true }
+        {
+            title: '操作', key: 'operate', onClick: function (index, row, data) {
+                _.OpenPage({
+                    id: 108001,
+                    title: '编辑凭证模板',
+                    url: "JSGL/VOUCHER/VoucherEdit/" + row.VOUCHERID
+                });
+            }
+        }
     ];
-    search.screenParam.colDef = col.concat(search.colOperate).concat(search.colMul);
     search.service = "CwglService";
     search.method = "GetVoucherList";
 }
-
-search.browseHref = function (row, index) {
-    _.OpenPage({
-        id: -108001,
-        title: '浏览凭证模板',
-        url: "JSGL/VOUCHER/VoucherEdit/" + row.VOUCHERID
-    });
-}
-
 search.addHref = function (row) {
     _.OpenPage({
         id: 108001,
         title: '新增凭证模板',
         url: "JSGL/VOUCHER/VoucherEdit/"
-    });
-}
-search.modHref = function (row, index) {
-    _.OpenPage({
-        id: 108001,
-        title: '编辑凭证模板',
-        url: "JSGL/VOUCHER/VoucherEdit/" + row.VOUCHERID
     });
 }
 
