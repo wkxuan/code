c939bbfddefc49d87835e608ea7adfaa728b6799
刘银桥 <@DESKTOP-0QFCLD2>
2019-08-26 11:19:23 +0800

    修改角色定义增加楼层权限

---
+++ z.ERP/trunk/z.ERP.Entities/Auto/ROLE_FLOOREntity.cs
@@ -0,0 +1,37 @@
+﻿using z.DBHelper.DBDomain;
+
+namespace z.ERP.Entities
+{
+    [DbTable("ROLE_FLOOR", "角色-楼层权限")]
+    public partial class ROLE_FLOOREntity : TableEntityBase
+    {
+        public ROLE_FLOOREntity()
+        {
+        }
+
+        public ROLE_FLOOREntity(string roleid, string floorid)
+        {
+            ROLEID = roleid;
+            FLOORID = floorid;
+        }
+
+        /// <summary>
+        /// 角色编码
+        /// <summary>
+        [PrimaryKey]
+        [Field("角色编码")]
+        public string ROLEID
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 区域ID
+        /// <summary>
+        [PrimaryKey]
+        [Field("楼层ID")]
+        public string FLOORID
+        {
+            get; set;
+        }
+    }
+}
