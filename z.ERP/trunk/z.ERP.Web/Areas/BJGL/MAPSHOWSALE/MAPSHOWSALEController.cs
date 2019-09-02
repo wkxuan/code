595422c1d20099acf8ea6ea7d171b58919d07934
董泽恺 <83956250@qq.com>
2019-09-02 15:35:54 +0800

    布局图销售分析

---
+++ z.ERP/trunk/z.ERP.Web/Areas/BJGL/MAPSHOWSALE/MAPSHOWSALEController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using z.ERP.Web.Areas.Base;
+using z.MVC5.Results;
+
+namespace z.ERP.Web.Areas.BJGL.MAPSHOWSALE
+{
+    public class MAPSHOWSALEController : BaseController
+    {
+        public ActionResult MAPSHOWSALE()
+        {
+            ViewBag.Title = "布局销售信息";
+            return View();
+        }
+        public UIResult GetInitMAPDATA(string floorid, string shopstatus, string starttime, string endtime)
+        {
+            var datas = service.DpglService.GetInitMAPDATA(floorid, shopstatus);
+            var saledata = service.DpglService.GETFLOORSALELIST(floorid, starttime, endtime);
+            return new UIResult(
+                new
+                {
+                    floorInfo = datas.Item1,
+                    labelArray = datas.Item2,
+                    salelist = saledata
+                }
+                );
+        }
+        public UIResult GetSHOPSALE(string shopid,string starttime,string endtime)
+        {
+            var data = service.DpglService.GetSHOPSALE(shopid, starttime,endtime);
+            return new UIResult(
+                new
+                {
+                    shopdata = data.Item1,
+                    shopsalelistY = data.Item2.AsEnumerable().Select<DataRow, decimal>(x => Convert.ToDecimal(x["AMOUNT"])).ToList<decimal>(),
+                    shopsalelistX = data.Item2.AsEnumerable().Select<DataRow, string>(x => Convert.ToString(x["RQS"])).ToList<string>(),
+                });
+        }
+    }
+}
