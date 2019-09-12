2afec35dc9ee128b6ac055bbcfaef8da5ecc5af5
董泽恺 <83956250@qq.com>
2019-09-12 16:06:55 +0800

    终端监控

---
+++ z.ERP/trunk/z.ERP.Entities/STATIONMONITOR.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z.ERP.Entities
+{
+    public class STATIONMONITOR
+    {
+        public string BRANCHID { set; get; }
+        public string BRANCHNAME { set; get; }
+        public List<STATIONINFO> STATIONINFOList { set; get; }
+    }
+    public class STATIONINFO {
+        public string STATIONBH { set; get; }
+        public string CASHIERNAME { set; get; }
+        public int STATIONSTATUS { set; get; }
+        public int GAPTIME { set; get; }
+        public string REFRESHTIME { set; get; }
+    }
+}
