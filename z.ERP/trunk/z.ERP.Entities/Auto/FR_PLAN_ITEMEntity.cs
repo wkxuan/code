b9a2b3d0a7db853e2a8fc0a46c44cbe3a46e4fba
董泽恺 <83956250@qq.com>
2019-09-17 17:00:50 +0800

    满减方案

---
+++ z.ERP/trunk/z.ERP.Entities/Auto/FR_PLAN_ITEMEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using z.DBHelper.DBDomain;
+
+namespace z.ERP.Entities
+{
+    [DbTable("FR_PLAN_ITEM", "满减方案子表")]
+    public partial class FR_PLAN_ITEMEntity: TableEntityBase
+    {
+        public FR_PLAN_ITEMEntity(){}
+        public FR_PLAN_ITEMEntity(string id,string inx) {
+            ID = id;
+            INX = inx;
+        }
+        /// <summary>
+        /// 规则id
+        /// <summary>
+        [PrimaryKey]
+        [Field("规则id")]
+        public string ID
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 序号
+        /// <summary>
+        [PrimaryKey]
+        [Field("序号")]
+        public string INX
+        {
+            get; set;
+        }
+        [Field("满额")]
+        public string FULL
+        {
+            get; set;
+        }
+        [Field("减额")]
+        public string CUT
+        {
+            get; set;
+        }
+    }
+}
---
+++ z.ERP/trunk/z.ERP.Entities/FR_PLANEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using z.DBHelper.DBDomain;
+
+namespace z.ERP.Entities
+{
+    public partial class FR_PLANEntity
+    {
+        [ForeignKey(nameof(ID), nameof(FR_PLAN_ITEMEntity.ID))]
+        public List<FR_PLAN_ITEMEntity> FR_PLAN_ITEM
+        {
+            get;
+            set;
+        }
+    }
+}
