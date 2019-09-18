82591b18ebe814a1278da2bff0bc3911a5993792
Elsa <Elsa@DESKTOP-PVBS06P>
2019-09-18 12:00:52 +0800

    Revert "添加赠品定义表"
    This reverts commit 42120ae9d0f47de88126775aa00cc9ff7ee183ee.

--- z.ERP/trunk/z.ERP.Services/CxglService.cs
+++ z.ERP/trunk/z.ERP.Services/CxglService.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using z.Encryption;
 using z.ERP.Entities;
 using z.ERP.Entities.Enum;
 using z.Extensions;
-using z.ERP.Model.Vue;
-using z.Exceptions;
 using z.MVC5.Results;
-using z.SSO.Model;
 
 namespace z.ERP.Services
 {
@@ -44,62 +39,5 @@ namespace z.ERP.Services
             var res = DbHelper.ExecuteOneObject<PROMOTIONEntity>(sql);
             return res;
         }
-
-        /// <summary>
-        /// 赠品定义
-        /// </summary>
-        /// <param name="item"></param>
-        /// <returns></returns>
-        public DataGridResult PresentSql(SearchItem item)
-        {
-            string sql = $@"SELECT BRANCHID, BRANCH.NAME, ID, NAME, PRICE, STATUS
-                            FROM PRESENT,BRANCH
-                            WHERE BRANCH.ID=PRESENT.BRANCHID";
-            sql += "  AND PRESENT.BRANCHID in (" + GetPermissionSql(PermissionType.Branch) + ")";  //门店权限
-            item.HasKey("BRANCHID", a => sql += $" and BRANCHID LIKE '%{a}%'");
-            item.HasKey("ID", a => sql += $" and ID LIKE '%{a}%'");
-            item.HasKey("NAME", a => sql += $" and NAME LIKE '%{a}%'");
-            item.HasKey("PRICE", a => sql += $" and PRICE LIKE '%{a}%'");
-            item.HasKey("STATUS", a => sql += $" and STATUS LIKE '%{a}%'");
-            sql += " ORDER BY ID DESC";
-            int count;
-            var dt = DbHelper.ExecuteTable(sql, item.PageInfo, out count);
-            dt.NewEnumColumns<促销单状态>("STATUS", "STATUSMC");
-            return new DataGridResult(dt, count);
-
-          
-
-        }
-        public DataGridResult Present(SearchItem item)
-        {
-            string sql = "";
-            int count;
-            var dt = DbHelper.ExecuteTable(sql, item.PageInfo, out count);
-            return new DataGridResult(dt, count);
-        }
-
-        public DataTable PresentDetail(SearchItem item)
-        {
-            //DataGridResult sql = PresentSql(item);
-            string sql = "";
-            DataTable dt = DbHelper.ExecuteTable(sql);
-        
-            return dt;
-        }
-        public DataTable GetPresent(PresentEntity data)
-        {
-            string sql = "";
-            string yTQx = GetPermissionSql(PermissionType.Category);
-            
-            //string sql = PresentSql(item);
-            //    $@"SELECT BRANCHID, BRANCH.NAME, HEAD, TAIL, ADQRCODE, ADCONTENT
-            //                    FROM PRESENT,BRANCH
-            //                    WHERE BRANCH.ID=PRESENT.BRANCHID";
-            //sql += "  AND TICKETINFO.BRANCHID in (" + GetPermissionSql(PermissionType.Branch) + ")";  //门店权限
-            sql += " and Present.ID=" + data.ID;
-            DataTable dt = DbHelper.ExecuteTable(sql);
-            return dt;
-        }
-
     }
 }
