c76e45ab12598c2d7e880a33dc87b4dc86584c13
刘银桥 <@DESKTOP-0QFCLD2>
2019-09-20 11:40:28 +0800

    tree模板bug修改

--- z.ERP/trunk/z.ERP.Web/Areas/XTGL/CATEGORY/Category.js
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/CATEGORY/Category.js
@@ -3,21 +3,25 @@
     define.method = "TreeCategoryData";
     define.methodList = "TreeCategoryList";
     define.Key = 'CATEGORYCODE';
-    define.dataParam.COLOR = " ";
-
+}
+define.initDataParam = function () {
     define.dataParam.CATEGORYCODE = null;
     define.dataParam.CATEGORYNAME = null;
     define.dataParam.LEVEL_LAST = null;
+    define.dataParam.COLOR = " ";
+}
+define.newRecord = function () {
+    define.dataParam.LEVEL_LAST = 1;
 }
 define.IsValidXj = function () {
-    if (define.dataParam.LEVEL_LAST == 2) {
+    if (define.vueObj.dataParam.LEVEL_LAST == 2) {
         iview.Message.info("该节点已是末级节点不能再添加下级节点！");
         return false;
     }
     return true;
 }
 define.IsValidDel = function () {
-    if (define.dataParam.LEVEL_LAST == 1) {
+    if (define.vueObj.dataParam.LEVEL_LAST == 1) {
         iview.Message.info("该节点是非末级节点不能删除！");
         return false;
     }
--- z.ERP/trunk/z.ERP.Web/Areas/XTGL/ORG/Org.js
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/ORG/Org.js
@@ -3,12 +3,6 @@
     define.method = "TreeOrgData";
     define.methodList = "TreeOrgList";
     define.Key = 'ORGCODE';
-
-    define.dataParam.ORGCODE = null;
-    define.dataParam.ORGNAME = null;
-    define.dataParam.ORG_TYPE = null;
-    define.dataParam.LEVEL_LAST = null;
-    define.dataParam.VOID_FLAG = null;
 }
 
 define.newRecord = function () {
@@ -16,15 +10,23 @@ define.newRecord = function () {
     define.dataParam.LEVEL_LAST = 1;
 }
 
+define.initDataParam = function () {
+    define.dataParam.ORGCODE = null;
+    define.dataParam.ORGNAME = null;
+    define.dataParam.ORG_TYPE = null;
+    define.dataParam.LEVEL_LAST = null;
+    define.dataParam.VOID_FLAG = null;
+}
+
 define.IsValidXj = function () {
-    if (define.dataParam.LEVEL_LAST == 2) {
+    if (define.vueObj.dataParam.LEVEL_LAST == 2) {
         iview.Message.info("该节点已是末级节点不能再添加下级节点！");
         return false;
     }
     return true;
 }
 define.IsValidDel = function () {
-    if (define.dataParam.LEVEL_LAST == 1) {
+    if (define.vueObj.dataParam.LEVEL_LAST == 1) {
         iview.Message.info("该节点是非末级节点不能删除！");
         return false;
     }
