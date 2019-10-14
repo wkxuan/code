4f4fe36e735e994392cd1b6451ebbb53a786165f
王凯旋 <wang.kx@live.cn>
2019-10-14 12:20:35 +0800

    支付方式错误提示问题

--- z.ERP/trunk/z.ERP.Web/Areas/XTGL/PAY/Pay.js
+++ z.ERP/trunk/z.ERP.Web/Areas/XTGL/PAY/Pay.js
@@ -14,47 +14,56 @@
     define.methodList = "GetPay";
     define.Key = 'PAYID';
 
+    define.dataParam.PAYID = "";
+    define.dataParam.VOID_FLAG = "";
+    define.dataParam.NAME = "";
+    define.dataParam.TYPE = "";
+    define.dataParam.FK = "";
+    define.dataParam.JF = "";
+    define.dataParam.ZLFS = "";
+    define.dataParam.FLAG = "";
+
 }
 
 define.newRecord = function () {
-    define.dataParam.VOID_FLAG = "1";
+    define.myve.dataParam.VOID_FLAG = 1;
 }
 
 
 define.IsValidSave = function () {
-    if (!define.dataParam.NAME) {
+    if (!define.myve.dataParam.NAME) {
         iview.Message.info("名称不能为空!");
         return false;
     }
-    if (!define.dataParam.TYPE) {
+    if (!define.myve.dataParam.TYPE) {
         iview.Message.info("类型不能为空!");
         return false;
     }
 
-    if (!define.dataParam.FK) {
+    if (!define.myve.dataParam.FK) {
         iview.Message.info("返款标记不能为空!");
         return false;
     }
-    if (!define.dataParam.JF) {
+    if (!define.myve.dataParam.JF) {
         iview.Message.info("积分标记不能为空!");
         return false;
     }
 
-    if (define.dataParam.ZLFS==null) {
+    if (define.myve.dataParam.ZLFS == null || define.myve.dataParam.ZLFS == undefined) {
         iview.Message.info("找零方式不能为空!");
         return false;
     }
 
-    if (!define.dataParam.FLAG) {
+    if (!define.myve.dataParam.FLAG) {
         iview.Message.info("显示序号不能为空!");
         return false;
     }
-    if (isNaN(define.dataParam.FLAG)) {
+    if (isNaN(define.myve.dataParam.FLAG)) {
         iview.Message.info("显示序号必须为数字!");
         return false;
     }
 
-    if (define.dataParam.TYPE == "3" && !define.dataParam.COUPONID)
+    if (define.myve.dataParam.TYPE == "3" && !define.myve.dataParam.COUPONID)
     {
         iview.Message.info("类型是优惠券时必须选择对应优惠券!");
         return false;
