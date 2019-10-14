aae58458ca67381e29e84c1495307f38fce9b4c5
董泽恺 <83956250@qq.com>
2019-10-14 16:41:37 +0800

    手动生成结算单

---
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/ManualReturn/ManualReturn.js
@@ -0,0 +1,24 @@
+﻿var MR = new Vue({
+    el: "#ManualReturn",
+    data: {
+        BRANCHID: "",
+        ENDTIME: "",
+    },
+    methods: {
+        Exec: function () {
+            if (!MR.BRANCHID) {
+                iview.Message.info("请选择门店!");
+                return false
+            }
+            if (!MR.ENDTIME) {
+                iview.Message.info("请选择截至时间!");
+                return false
+            }
+            _.Ajax('ExecReturn', {
+                branchid: MR.BRANCHID, endtime: MR.ENDTIME
+            }, function (data) {
+                debugger
+            });
+        }
+    },   
+});
---
+++ z.ERP/trunk/z.ERP.Web/Areas/JSGL/ManualReturn/ManualReturnController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using z.ERP.Web.Areas.Base;
+
+namespace z.ERP.Web.Areas.JSGL.ManualReturn
+{
+    public class ManualReturnController: BaseController
+    {
+        public ActionResult ManualReturn()
+        {
+            ViewBag.Title = "手动生成返款单";
+            return View();
+        }
+        public bool ExecReturn(string branchid,string endtime)
+        {
+            return true;
+        }
+    }
+}
